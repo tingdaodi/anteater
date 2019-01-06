df52b59fcb5c2f776d1e39ecfee7520489d39c3f
tingdaodi <11264697+tingdaodi@users.noreply.github.com>
2019-01-06 23:17:58 +0800

    Init project

---
+++ anteater-payment-discrete-center/src/main/java/com/pn/anteater/payment/config/SwaggerConfiguration.java
@@ -0,0 +1,11 @@
+package com.pn.anteater.payment.config;
+
+/**
+ * Description TODO
+ *
+ * @author Roye.L
+ * @date 2019/1/6 22:38
+ * @since 1.0
+ */
+public class SwaggerConfiguration {
+}
---
+++ anteater-payment-discrete-center/src/main/java/com/pn/anteater/payment/controller/User.java
@@ -0,0 +1,11 @@
+package com.pn.anteater.payment.controller;
+
+/**
+ * Description TODO
+ *
+ * @author Roye.L
+ * @date 2019/1/6 22:05
+ * @since 1.0
+ */
+public class User {
+}
