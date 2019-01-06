75c6d2ddc5948f5aa62436df12faa93071cf214a
tingdaodi <11264697+tingdaodi@users.noreply.github.com>
2019-01-06 21:40:53 +0800

    add anteater-payment-discrete-center

---
+++ anteater-payment-discrete-center/src/main/java/com/pn/anteater/payment/AnteaterPaymentDiscreteCenterApplication.java
@@ -0,0 +1,14 @@
+package com.pn.anteater.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnteaterPaymentDiscreteCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnteaterPaymentDiscreteCenterApplication.class, args);
+    }
+
+}
+
---
+++ anteater-payment-discrete-center/src/test/java/com/pn/anteater/payment/AnteaterPaymentDiscreteCenterApplicationTests.java
@@ -0,0 +1,17 @@
+package com.pn.anteater.payment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AnteaterPaymentDiscreteCenterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
